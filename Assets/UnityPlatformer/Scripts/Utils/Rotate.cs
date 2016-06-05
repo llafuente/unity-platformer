9af7c237a97ae4a967dd1b2844f03381d05e1c3a
llafuente <llafuente@noboxout.com>
2016-06-05 16:16:09 +0200

    move staff to prepare to create unitypackage

--- Assets/UnityPlatformer/Scripts/Utils/Rotate.cs
+++ Assets/UnityPlatformer/Scripts/Utils/Rotate.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace UnityPlatformer {
+  /// <summary>
+  /// Rotate current object
+  /// </summary>
   public class Rotate : MonoBehaviour {
     [Comment("In Degrees")]
     public Vector3 rotationPerSecond;
