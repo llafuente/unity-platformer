c8b1bea1c407738a94c62761ac0a20d71b640b26
llafuente <llafuente@noboxout.com>
2016-06-30 11:24:23 +0200

    Support walk/run on ground/air movement

--- Assets/UnityPlatformer/Scripts/Actions/CharacterActionAirMovement.cs
+++ Assets/UnityPlatformer/Scripts/Actions/CharacterActionAirMovement.cs
@@ -5,18 +5,7 @@ namespace UnityPlatformer {
   /// <summary>
   /// Movement while airbone, despite beeing falling, jumping...
   /// </summary>
-  public class CharacterActionAirMovement: CharacterAction {
-    #region public
-
-    [Comment("Movement speed")]
-    public float speed = 6;
-    [Comment("Time to reach max speed")]
-    public float accelerationTime = .2f;
-
-    #endregion
-
-    float velocityXSmoothing;
-
+  public class CharacterActionAirMovement: CharacterActionHorizontalMovement {
     /// <summary>
     /// Execute when no collision below.
     /// </summary>
@@ -27,32 +16,6 @@ namespace UnityPlatformer {
         return 0;
       }
       return -1;
-    }
-
-    /// <summary>
-    /// Reset SmoothDamp
-    /// </summary>
-    public override void GainControl(float delta) {
-      base.GainControl(delta);
-      velocityXSmoothing = 0;
-    }
-
-    /// <summary>
-    /// Horizontal movement
-    /// </summary>
-    public override void PerformAction(float delta) {
-      float targetVelocityX = input.GetAxisRawX() * speed;
-
-      character.velocity.x = Mathf.SmoothDamp (
-        character.velocity.x,
-        targetVelocityX,
-        ref velocityXSmoothing,
-        accelerationTime
-      );
-    }
-
-    public override PostUpdateActions GetPostUpdateActions() {
-      return PostUpdateActions.WORLD_COLLISIONS | PostUpdateActions.APPLY_GRAVITY;
     }
   }
 }
--- Assets/UnityPlatformer/Scripts/Actions/CharacterActionGroundMovement.cs
+++ Assets/UnityPlatformer/Scripts/Actions/CharacterActionGroundMovement.cs
@@ -5,18 +5,7 @@ namespace UnityPlatformer {
   /// <summary>
   /// Movement while on ground and not slipping
   /// </summary>
-  public class CharacterActionGroundMovement: CharacterAction {
-    #region public
-
-    [Comment("Movement speed")]
-    public float speed = 6;
-    [Comment("Time to reach max speed")]
-    public float accelerationTime = .1f;
-
-    #endregion
-
-    float velocityXSmoothing;
-
+  public class CharacterActionGroundMovement: CharacterActionHorizontalMovement {
     /// <summary>
     /// Execute when collision below.
     /// </summary>
@@ -29,40 +18,6 @@ namespace UnityPlatformer {
         return -1;
       }
       return 0;
-    }
-
-    /// <summary>
-    /// Reset SmoothDamp
-    /// </summary>
-    public override void GainControl(float delta) {
-      base.GainControl(delta);
-      velocityXSmoothing = 0;
-    }
-
-
-    /// <summary>
-    /// Do horizontal movement
-    /// </summary>
-    public override void PerformAction(float delta) {
-      Move(speed, ref velocityXSmoothing, accelerationTime);
-    }
-
-    /// <summary>
-    /// Horizontal movement based on current input
-    /// </summary>
-    public void Move(float spdy, ref float smoothing, float accTime) {
-      float targetVelocityX = input.GetAxisRawX() * spdy;
-
-      character.velocity.x = Mathf.SmoothDamp (
-        character.velocity.x,
-        targetVelocityX,
-        ref velocityXSmoothing,
-        accTime
-      );
-    }
-
-    public override PostUpdateActions GetPostUpdateActions() {
-      return PostUpdateActions.WORLD_COLLISIONS | PostUpdateActions.APPLY_GRAVITY;
     }
   }
 }
